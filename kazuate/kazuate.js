9afdc651d7fbd154f8b848286b16534d9534ebc5
G184962021 <218496@st.takushoku-u.ac.jp>
2022-06-23 15:36:49 +0900

    doit

---
+++ kazuate/kazuate.js
@@ -0,0 +1,40 @@
+let kotae = Math.floor(Math.random()*10) + 1;
+console.log('答え: ' + kotae);      // デバッグ用
+
+// 入力回数（予想回数）
+let kaisu = 0;
+let a=0;
+// 予想を4回実行する
+// 将来: ボタンを押したら， hantei() を呼び出すように修正する
+hantei();
+hantei();
+hantei();
+hantei();
+
+// ボタンを押した後の処理をする関数 hantei() の定義
+function hantei() {
+    let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
+    // 課題3-1：ここの判定処理を作成する．
+    kaisu = kaisu + 1;
+    console.log(kaisu + "回目の予想:" + yoso);
+    if(kaisu < 4){
+    if(yoso < kotae){
+        console.log("まちがい．答えはもっと大きいですよ");
+    }else if(yoso > kotae){
+        console.log("まちがい．答えはもっと小さいですよ");
+    }else {
+        if(a===0){
+        console.log("正解です．おめでとう!");
+        a++;
+         }else{
+            console.log("答えは ◯ でした．すでにゲームは終わっています")
+         }
+       }
+       }else{
+        console.log("まちがい．残念でした答えは "+kotae+" です．");
+         }
+        
+    //        ページに表示する方法はまだ習っていないので
+    //        判定結果はコンソールに出力すること
+
+}
